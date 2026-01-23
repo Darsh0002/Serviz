9d992226cddfc73fd9b85e6bab15cd636c406a38
Darsh_Balar <darshbalar2005@gmail.com>
2026-01-23 16:10:18 +0530

    Created Backend Folder

---
+++ Serviz-Backend/src/main/java/com/darsh/Serviz_Backend/ServizBackendApplication.java
@@ -0,0 +1,13 @@
+package com.darsh.Serviz_Backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServizBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServizBackendApplication.class, args);
+	}
+
+}
---
+++ Serviz-Backend/src/test/java/com/darsh/Serviz_Backend/ServizBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.darsh.Serviz_Backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServizBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
