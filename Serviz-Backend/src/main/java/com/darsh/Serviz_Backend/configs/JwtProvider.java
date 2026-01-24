89967ba04989cdff8bb21972f24fbd54f0d8b039
Darsh_Balar <darshbalar2005@gmail.com>
2026-01-24 12:59:49 +0530

    Added JWT utility methods

---
+++ Serviz-Backend/src/main/java/com/darsh/Serviz_Backend/configs/JwtProvider.java
@@ -0,0 +1,39 @@
+package com.darsh.Serviz_Backend.configs;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import javax.crypto.SecretKey;
+import java.util.Date;
+
+public class JwtProvider {
+
+    static SecretKey key = Keys.hmacShaKeyFor("fhbejfbkernfvjrngjkungjknsvjusgntgbjkensamfbrvjkhjksrng,rk".getBytes());
+
+    public static String generateToken(Authentication auth) {
+
+        String role = auth.getAuthorities()
+                .stream()
+                .findFirst()
+                .map(GrantedAuthority::getAuthority)
+                .orElse("");
+
+        return Jwts.builder()
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + 86400000)) // 1 day
+                .claim("email", auth.getName())
+                .claim("role", role)
+                .signWith(key)
+                .compact();
+    }
+
+    public static String getEmailFromToken(String jwt) {
+        if (jwt != null && jwt.startsWith("Bearer ")) {
+            jwt = jwt.substring(7);
+        }
+        Claims claims = Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(jwt).getBody();
+        return String.valueOf(claims.get("email"));
+    }
+}
