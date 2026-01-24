dadc1585ff57ea391be5acf55158c22f3e00c0cb
Darsh_Balar <darshbalar2005@gmail.com>
2026-01-24 23:38:43 +0530

    Configured Routes

--- Serviz-Frontend/src/App.jsx
+++ Serviz-Frontend/src/App.jsx
@@ -1,9 +1,40 @@
-import React from 'react'
+import React from "react";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { Toaster } from "react-hot-toast";
+
+import ProtectedRoute from "./routes/ProtectedRoute";
+
+import Login from "./pages/LoginPage";
+import UserDashboard from "./pages/UserDashboard";
+import ProviderDashboard from "./pages/ProviderDashboard";
+import Unauthorized from "./pages/Unauthorized";
+import LandingPage from "./pages/LandingPage";
 
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <BrowserRouter>
+      <Toaster position="top-center" />
 
-export default App
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/unauthorized" element={<Unauthorized />} />
+
+        {/* USER ROUTES */}
+        <Route element={<ProtectedRoute allowedRoles={["USER"]} />}>
+          <Route path="/user/dashboard" element={<UserDashboard />} />
+        </Route>
+
+        {/* PROVIDER ROUTES */}
+        <Route element={<ProtectedRoute allowedRoles={["PROVIDER"]} />}>
+          <Route path="/provider/dashboard" element={<ProviderDashboard />} />
+        </Route>
+
+        {/* FALLBACK */}
+        <Route path="*" element={<Navigate to="/login" />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default App;
